2f1ed17cd72c221ee0aa98f384691fd830256210
Varun <varunsridharan23@gmail.com>
2020-04-17 16:49:56 +0530

    Updated Action

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eo
+
+$docs_repo_link = $INPUT_docs_repo_link
+$docs_change_log_file = $INPUT_docs_change_log_file
+$local_change_log_file = $INPUT_local_change_log_file
+
+echo $docs_repo_link
+echo $docs_change_log_file
+echo $local_change_log_file
+
+# Update Github Config.
+#git config --global user.email "githubactionbot+changelog@gmail.com" && git config --global user.name "Change Log Updater Github Bot"
+
+# Add File To Commit
+#git add -A
+
+# Add Commit Message
+#git commit -m "Change Log Updated"
+
+# Push To Github
+#git push "https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY" HEAD:$BRANCH
