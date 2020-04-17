18aa75b166385b8d3b1a9f61355fa43659f1e80f
Varun <varunsridharan23@gmail.com>
2020-04-17 18:40:28 +0530

    Out for vacation... DONT YOU DARE TO CALL ME.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,31 +6,30 @@ DOCS_REPO_NAME="${INPUT_REPO_NAME}"
 DOCS_REPO_LINK="https://github.com/${DOCS_REPO_NAME}"
 DOCS_CHANGE_LOG_FILE="${INPUT_REMOTE_FILE}"
 DOCS_LOCAL_CHANGE_LOG_FILE="${INPUT_LOCAL_FILE}"
-echo "Vars"
-echo "DOCS_CHANGE_LOG_FILE : ${DOCS_CHANGE_LOG_FILE}"
-echo "DOCS_LOCAL_CHANGE_LOG_FILE : ${DOCS_LOCAL_CHANGE_LOG_FILE}"
-echo "DOCS_REPO_NAME : ${DOCS_REPO_NAME}"
 
-echo "Clone Remote Github Repository"
+echo "Remote File : ${DOCS_CHANGE_LOG_FILE}"
+echo "Local File : ${DOCS_LOCAL_CHANGE_LOG_FILE}"
+
+echo "Setting Up!"
 cd ../../
 mkdir docs_repo
 cd docs_repo
+
+echo "Clone Documentation Github Repository"
 git clone $DOCS_REPO_LINK ./
 
-echo "Copying File"
+echo "Copying Change Log File"
 rm -rf $remote_change_log_file
-cat ./$DOCS_CHANGE_LOG_FILE
 cp $GITHUB_WORKSPACE/$DOCS_LOCAL_CHANGE_LOG_FILE ./$DOCS_CHANGE_LOG_FILE
 
-
 # Update Github Config.
-#git config --global user.email "githubactionbot+changelog@gmail.com" && git config --global user.name "Change Log Updater Github Bot"
+git config --global user.email "githubactionbot+changelog@gmail.com" && git config --global user.name "Change Log Updater Github Bot"
 
 # Add File To Commit
-#git add -A
+git add -A
 
 # Add Commit Message
-#git commit -m "Change Log Updated"
+git commit -m "Change Log Updated"
 
 # Push To Github
-#git push "https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY" HEAD:$BRANCH
+git push "https://x-access-token:$GITHUB_TOKEN@github.com/$DOCS_REPO_NAME" HEAD:$BRANCH
