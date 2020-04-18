85a38ceabeb304a5ac1a5505e677434c62110439
Varun <varunsridharan23@gmail.com>
2020-04-18 07:50:33 +0530

    Updated.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,16 @@ DOCS_CHANGE_LOG_FILE="${INPUT_REMOTE_FILE}"
 DOCS_LOCAL_CHANGE_LOG_FILE="${INPUT_LOCAL_FILE}"
 GITHUB_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-echo "Setting Up!"
+echo "
+Setting Up!
+"
 cd ../../
 mkdir docs_repo
 cd docs_repo
 
-echo "Clone Documentation Github Repository"
+echo "
+Clone Documentation Github Repository
+"
 git clone $DOCS_REPO_LINK ./
 
 echo "
@@ -21,16 +25,19 @@ Remote File : ${DOCS_CHANGE_LOG_FILE}
 Local  File : ${DOCS_LOCAL_CHANGE_LOG_FILE}
 "
 
-echo "Copying Change Log File"
+echo "
+Copying Change Log File
+"
+
 rm -rf $DOCS_CHANGE_LOG_FILE
 cp $GITHUB_WORKSPACE/$DOCS_LOCAL_CHANGE_LOG_FILE ./$DOCS_CHANGE_LOG_FILE
-
-something_changed=`git diff-index --exit-code --ignore-submodules HEAD`
 
 
 if [ -z "$(git status --porcelain)" ]; then
     # Working directory is clean
-    echo ">>> No changes detected "
+    echo "
+    >>> No changes detected
+    "
 else
     # Update Github Config.
     git config --global user.email "githubactionbot+changelog@gmail.com" && git config --global user.name "Change Log Updater Github Bot"
@@ -43,4 +50,8 @@ else
 
     # Push To Github
     git push "https://x-access-token:$GITHUB_TOKEN@github.com/$DOCS_REPO_NAME"
+
+    echo "
+    Copying complete 👌
+    "
 fi
