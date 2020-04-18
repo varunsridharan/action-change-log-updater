3f4178fc0fbd1166cb2c77ecaae63cee9ca4b4d3
Varun <varunsridharan23@gmail.com>
2020-04-18 07:48:52 +0530

    Fixed. Handler.

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,19 +27,20 @@ cp $GITHUB_WORKSPACE/$DOCS_LOCAL_CHANGE_LOG_FILE ./$DOCS_CHANGE_LOG_FILE
 
 something_changed=`git diff-index --exit-code --ignore-submodules HEAD`
 
-if [ -n "$something_changed" ]
-  echo ">>> Nothing To Update"
-then
-   # Update Github Config.
-  git config --global user.email "githubactionbot+changelog@gmail.com" && git config --global user.name "Change Log Updater Github Bot"
 
-  # Add File To Commit
-  git add -A
+if [ -z "$(git status --porcelain)" ]; then
+    # Working directory is clean
+    echo ">>> No changes detected "
+else
+    # Update Github Config.
+    git config --global user.email "githubactionbot+changelog@gmail.com" && git config --global user.name "Change Log Updater Github Bot"
 
-  # Add Commit Message
-  git commit -m "Change Log Updated"
+    # Add File To Commit
+    git add -A
 
-  # Push To Github
-  git push "https://x-access-token:$GITHUB_TOKEN@github.com/$DOCS_REPO_NAME"
+    # Add Commit Message
+    git commit -m "Change Log Updated"
 
+    # Push To Github
+    git push "https://x-access-token:$GITHUB_TOKEN@github.com/$DOCS_REPO_NAME"
 fi
