6427b1153d6130e386fa87a50aa649fd9b93b013
Varun <varunsridharan23@gmail.com>
2020-04-29 18:21:11 +0530

    Updated Log

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,10 @@ DOCS_REPO_LINK="https://x-access-token:$GITHUB_TOKEN@github.com/${DOCS_REPO_NAME
 
 echo "
 ------------------------------------------------
-Repo Name : ${DOCS_REPO_NAME}                  |
-Repo Link : ${DOCS_REPO_LINK}                  |
-Remote File : ${DOCS_CHANGE_LOG_FILE}          |
-Local  File : ${DOCS_LOCAL_CHANGE_LOG_FILE}    |
+Repo Name : ${DOCS_REPO_NAME}
+Repo Link : ${DOCS_REPO_LINK}
+Remote File : ${DOCS_CHANGE_LOG_FILE}
+Local  File : ${DOCS_LOCAL_CHANGE_LOG_FILE}
 ------------------------------------------------
 "
 
@@ -33,10 +33,12 @@ cp $GITHUB_WORKSPACE/$DOCS_LOCAL_CHANGE_LOG_FILE ./$DOCS_CHANGE_LOG_FILE
 if [ -z "$(git status --porcelain)" ]; then
   echo "✅ Nothing To Push"
 else
+  echo "##[group] 👌 Updating ${DOCS_REPO_NAME}"
   git config --global user.email "githubactionbot+changelog@gmail.com" && git config --global user.name "Change Log Updater Github Bot"
   git add -A
   git commit -m "👌 Change Log Updated / ⚡ Triggered By $GITHUB_SHA"
   #git push "https://x-access-token:$GITHUB_TOKEN@${DOCS_REPO_LINK}"
   git push ${DOCS_REPO_LINK}
-  echo "👌 Update Complete"
+  echo "##[endgroup]
+  👌 Update Complete"
 fi
